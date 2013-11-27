c0f707af7280254f1eccd2286b16481c5236cc2c
semerj <jsemer@gmail.com>
2013-11-26 22:13:15 -0800

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,6 +1,4 @@
 wget http://cdph.ca.gov/programs/hai/Documents/2012-CLABSI-T{03..40}.pdf
 mkdir output
-mkdir text
 for f in *.pdf; do qpdf --password= --decrypt $f output/out-$f; done
-cd output/
-for f in *.pdf; do pdf2txt.py -o ../text/${f%.pdf}.txt $f; done
+for f in output/*.pdf; do pdftotext -layout $f; done

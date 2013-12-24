05be7f77c4ffdb767159d842f100d6f237dcaee3
semerj <jsemer@gmail.com>
2013-12-24 14:26:58 -0800

    Update clabsi2012.py

--- clabsi2012.py
+++ clabsi2012.py
@@ -8,26 +8,26 @@ def clip(files):
     data = pd.DataFrame()
     units = []
     for f in files:
-        df = [re.split(r'\s{2,}', d.strip()) for d in f if re.findall(r'[A-Z]{5}', d)]
+        df = [re.split(r'\s{2,}|\,(?=\d)', re.sub('[()]', '', d.strip())) for d in f if re.findall(r'[A-Z]{5}', d)]
         df = pd.DataFrame(df)
         matched = re.findall(r'providing ([^]]*), reported', " ".join(f[0:2]).replace('\n', ''))
         if matched:
             units.append(matched[0])
             df['type'] = matched[0]
         data = data.append(df)
-    data.columns = ['hosp', 'cases', 'lineDays', 'rate', 'ci', 'adherence', 'extra', 'area']
+    data.columns = ['hosp', 'cases', 'lineDays', 'rate', 'ciLow', 'ciHigh', 'adherence', 'extra', 'area']
     data.replace(to_replace='-------', value='', inplace=True)
     data.loc[data.extra.notnull(), 'adherence'] = data.loc[data.extra.notnull(), 'extra']
+    data.drop_duplicates(inplace=True)
     del data['extra']
-    return data
-    #data.to_csv('clip2012.csv', index=False)
+    data.to_csv('clip2012.csv', index=False)
     
     
 def clabsi(files):
     data = pd.DataFrame()
     units = []
     for f in files:
-        df = [re.split(r'\s{2,}', d.strip()) for d in f if re.findall(r'[A-Z]{5}', d)]
+        df = [re.split(r'\s{2,}|\,(?=\d)', re.sub('[()]', '', d.strip())) for d in f if re.findall(r'[A-Z]{5}', d)]
         df = pd.DataFrame(df)
         matched = re.findall(r'providing ([^]]*), reported', " ".join(f[0:2]).replace('\n', ''))
         if matched:
@@ -35,22 +35,16 @@ def clabsi(files):
             df['type'] = matched[0]
         data = data.append(df)
     
-    data.columns = ['hosp', 'cases', 'lineDays', 'sir', 'ci', 'extra', 'area']
+    data.columns = ['hosp', 'cases', 'lineDays', 'sir', 'ciLow', 'ciHigh', 'extra', 'area']
+    data.replace(to_replace='-------', value='', inplace=True)
+    data.drop_duplicates(inplace=True)
     del data['extra']
+    #data[data.duplicated()]
     data.to_csv('clabsi2012.csv', index=False)
-
-#    data['ci'] = data['ci'].str.replace('[(,)]', '')
-#    data['ci-low'] = data.ci.str.split().str[0]
-#    data['ci-high'] = data.ci.str.split().str[1]
-#    del data['ci']
-#    data = data[['hosp', 'procs', 'procCount', 'infCount', 'sir', 'ci-low', 'ci-high', 'comp']]
-    
-#    data.to_csv('clip2012.csv', index=False)    
-
 
 if __name__ == "__main__":
     filenames = glob.glob('*.txt')
     clipfiles = [open(name).readlines() for name in filenames[0:14]]
     clabsifiles = [open(name).readlines() for name in filenames[15:37]]
     clip(clipfiles)
-    #clabsi(clabsifiles)
+    clabsi(clabsifiles)
